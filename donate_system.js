6b046c4c8cb4512c2fc465af395b14d508ca96aa
vanya <vanadublanin@gmail.com>
2020-07-23 15:21:12 +0300

    destructuring

--- donate_system.js
+++ donate_system.js
@@ -15,44 +15,47 @@ const connection = mysql.createConnection({
     password: "forever16"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) {
         return console.error("Ошибка: " + err.message);
-    }
-    else{
+    } else {
         console.log("Подключение к серверу MySQL успешно установлено");
     }
 });
 
-const schemas =  Joi.object({
-        donater: Joi.string(),
-        amount: Joi.number(),
-        currency: Joi.string(),
-        streamer: Joi.string()
-    });
+const schemas = Joi.object({
+    donater: Joi.string(),
+    amount: Joi.number(),
+    currency: Joi.string(),
+    streamer: Joi.string()
+});
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({message: "Welcome to bezkoder application."});
 });
 
-app.get("/all_streamers",(req, res) => {
-    const sql = "SELECT * FROM personal_streamer_donations";
+app.get("/all_streamers", (req, res) => {
+    let {page,limit} = req.query;
+    const num = Number(page);
+    const gap = Number(limit);
+    const sql = `SELECT * FROM personal_streamer_donations LIMIT ${(num-1)*gap},${gap}`;
     const query = connection.query(sql, (err, results) => {
-        if(err) throw err;
+        if (err) throw err;
         res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
     });
+
 });
 
 
-app.get("/all_streamers/:id",(req,res) => {
+app.get("/all_streamers/:id", (req, res) => {
 
-    const sql = "SELECT SUM(amount),currency,streamer FROM personal_streamer_donations WHERE streamer=? GROUP BY currency" ;
+    const sql = "SELECT SUM(amount),currency,streamer FROM personal_streamer_donations WHERE streamer=? GROUP BY currency";
     const streamer = [req.params.id];
-    const query = connection.query(sql, streamer,(err, results) => {
-        if(err) throw err;
+    const query = connection.query(sql, streamer, (err, results) => {
+        if (err) throw err;
         res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
     });
 });
@@ -62,14 +65,13 @@ app.post(
     "/donate",
     validator.query(schemas),
     (req, res) => {
-    const form = [[req.body.donater,req.body.amount, req.body.currency, req.body.streamer]];
-    const  sql = "INSERT INTO donates(donater,amount,currency,streamer) VALUES ?";
-    const query = connection.query(sql, [form],(err, results) => {
-        if(err) throw err;
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        const form = [[req.body.donater, req.body.amount, req.body.currency, req.body.streamer]];
+        const sql = "INSERT INTO donates(donater,amount,currency,streamer) VALUES ?";
+        const query = connection.query(sql, [form], (err, results) => {
+            if (err) throw err;
+            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        });
     });
-});
-
 
 
 app.listen(3000, () => {
