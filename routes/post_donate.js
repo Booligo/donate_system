0e46b3e69ba87a3900e1f539dbb20010c5a3d6d0
vanya <vanadublanin@gmail.com>
2020-08-05 19:22:45 +0300

    update post

--- routes/post_donate.js
+++ routes/post_donate.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const post_values = require('../Functions/get_values');
-const our_values = require('../Functions/get_values');
 const validator = require('express-joi-validation').createValidator({});
 const Joi = require('joi');
 
