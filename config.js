048589ecde3d42f6479d1daae2b8448975ac0002
Ivan <vanadublanin@gmail.com>
2020-09-13 12:52:15 +0300

    fix config

---
+++ config.js
@@ -0,0 +1,13 @@
+const configuration = {
+    db_config : {
+        host: "localhost" || "localhost",
+        user: "root",
+        database: "donate",
+        password: "forever16"
+    },
+    redis_config: {
+        port: 6379 || 6379 ,
+        host: "127.0.0.1" || "127.0.0.1"
+    }
+};
+module.exports = {config:configuration};
